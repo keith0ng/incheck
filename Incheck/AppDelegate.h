462d893a731ae6de466570cf5d47cf8fccbf71f6
John Russel Usi <j.usi@klab.com>
2016-09-02 19:54:09 +0800

    Initial project setup Setup cocoa pods Added AFNetworking

---
+++ Incheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Incheck
+//
+//  Created by John Russel Usi on 9/2/16.
+//  Copyright © 2016 kkb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Incheck/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Incheck
+//
+//  Created by John Russel Usi on 9/2/16.
+//  Copyright © 2016 kkb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
