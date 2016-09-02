9acbb2ed4700a1f66b5e1a67735453f3e8a7a372
Keith Samson <keithmark_samson@yahoo.com>
2016-09-03 07:42:32 +0800

    Added Register View. Modified user model.

---
+++ Incheck/RegisterViewController.m
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.m
+//  Incheck
+//
+//  Created by John Russel Usi on 9/3/16.
+//  Copyright © 2016 kkb. All rights reserved.
+//
+
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+- (IBAction)registerUser:(id)sender {
+    
+    
+    
+}
+
+@end
